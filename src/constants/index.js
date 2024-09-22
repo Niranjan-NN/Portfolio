16a87abf6aa008e0d1dd4818128a8c64c436979f
Niranjan-NN <niranjan87901@gmail.com>
2024-09-22 18:49:20 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,69 @@
+import project1 from "../assets/projects/smart_ration.jpeg";
+import project2 from "../assets/projects/fieastaindiana.jpeg";
+import project3 from "../assets/projects/project-2.webp.jpg";
+import project4 from "../assets/projects/aara.jpeg";
+
+export const HERO_CONTENT = `I am a dedicated full-stack developer and aspiring data scientist with experience in crafting responsive and intuitive web applications. Having worked with technologies like React, Next.js, TypeScript, OpenCV, PowerBI, and MongoDB, I have developed projects that emphasize both functionality and user experience. My expertise extends to backend development, integrating databases like MongoDB, and utilizing machine learning in AI-driven solutions. With a strong foundation in full-stack development and a passion for problem-solving, I aim to create impactful solutions that enhance user engagement and contribute to business success.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Jun-2024 - Jul-2024",
+    role: "Data Scientist(Intern)",
+    company: "CodTech IT Solutions",
+    description: `Engaged in learning data analysis and machine learning concepts using Python and relevant libraries. Explored techniques for data visualization and predictive modeling, gaining insights into the practical applications of data science in business contexts. Collaborated with team members to understand project workflows and best practices in data-driven decision-making.`,
+    technologies: ["Python", "Jupyter Note-Book", "Python-Libraries", ],
+  },
+  {
+    year: "Jul-2024 - Aug-2024",
+    role: "Full Stack Developer(Intern)",
+    company: "Web-Gen",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Node.js", "MongoDB", "Apache"],
+  },
+  {
+    year: "Aug-2024 - Sep-2024",
+    role: "Data Scientist(Intern)",
+    company: "CodSoft",
+    description: `Focused on understanding core data science principles, including data analysis and machine learning methodologies. Participated in hands-on learning sessions that covered data visualization techniques and model development. Gained insights into real-world data applications and contributed to discussions on improving data-driven strategies within the organization.`,
+    technologies: ["Azure", "PowerBI", "Excell", "OpenCV"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Smart Ration",
+    image: project1,
+    description:
+      "An application designed for efficiently managing time slots at ration shops, allowing users to book appointments, track availability, and ensure a smooth shopping experience",
+    technologies: ["MIT App Inverter"],
+  },
+  {
+    title: "FieastaIndiana",
+    image: project2,
+    description:
+      "A comprehensive tourism website that facilitates booking hotels, hiring tourist guides, and ordering food, providing users with a seamless experience for planning and enjoying their travels.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "InnerPeaceAI",
+    image: project3,
+    description:
+      "An application designed to reduce stress by providing users with personalized ideas and strategies for relaxation and mindfulness. Features include mood assessment, tailored suggestions, guided exercises, and links to professional resources for further support.",
+    technologies: ["HTML", "CSS", "React", "MondoDB", "DialogueFlow"],
+  },
+  {
+    title: "AARA",
+    image: project4,
+    description:
+      "An image recognition chatbot that identifies images and generates relevant prompts, enabling users to interactively explore and understand visual content.",
+    technologies: ["HTML", "CSS", "React", "Python", "MongoDB"],
+  },
+];
+
+export const CONTACT = {
+  address: "7/10-2 Sri Ram Garden, No.4-Veerapandi, Coimbatore. ",
+  phoneNo: "+91 99444-56377 ",
+  email: "niranjan87901@gmail.com",
+};
