aa6d22ca81a88de7131d818ca085b8096080afa0
Niranjan-NN <129034340+Niranjan-NN@users.noreply.github.com>
2024-12-14 22:49:47 +0530

    Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import { PROJECTS } from "../constants"
 const Projects = () => {
   return (
     <div className="pb-4">
-        <h2 className="my-20 text-center text-4xl">Projeccts</h2>
+        <h2 className="my-20 text-center text-4xl">Projects</h2>
         <div>
            {PROJECTS.map((project, index) => (
                 <div key={index} className="mb-8 flex flex-warp lg:justify-center">
