3e3d7ba9d5ac09840845348ae7b7c1c86993559a
Niranjan-NN <niranjan87901@gmail.com>
2024-12-14 21:59:28 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/Niranjan-NN/Portfolio', // add this line to define the base URL
+  build: {
+    outDir: 'dist',
+  }
 })
