5ccfe98ba21b0002b76539aba8ca4cf1605d5e1e
Niranjan-NN <129034340+Niranjan-NN@users.noreply.github.com>
2025-01-01 14:30:28 +0530

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,13 +17,16 @@ const Hero = () => {
                 <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
                   {HERO_CONTENT}
                 </p>
-                <a href="/resume.pdf"
+                <a
+                    href="/resume.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                     download
-                    className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">
-                  Download Resume
-                </a>
+                    className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
+                  >
+                    Download Resume
+                  </a>
+
               </div>
           </div>
         </div>
